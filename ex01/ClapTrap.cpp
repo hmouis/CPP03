22a08d6ce4b4e303769b048f87947943fac91aaf
hmouis <houdayfamouis@gmail.com>
2025-12-22 16:05:09 +0100

    work with the attribute directly inside the member functions instead of getters and setters

--- ex01/ClapTrap.cpp
+++ ex01/ClapTrap.cpp
@@ -2,32 +2,31 @@
 
 ClapTrap::ClapTrap(){ std::cout << "Default constructor called\n";}
 
-ClapTrap::ClapTrap(std::string Name){
-    this->Name = Name;
-    this->HitPoints = 10;
-    this->EnergyPoints = 10;
-    this->AttackDamage = 0;
+ClapTrap::ClapTrap(std::string Name): Name(Name), HitPoints(10), EnergyPoints(10), AttackDamage(0)
+{
     std::cout << "Parameterized constructor called\n";
 }
 
 ClapTrap::ClapTrap(const ClapTrap& other)
 {
-    this->Name = other.Name;
-    this->HitPoints = other.HitPoints;
-    this->EnergyPoints = other.EnergyPoints;
-    this->AttackDamage = other.AttackDamage;
     std::cout << "Copy constructor called\n";
+    if (this != &other){
+        this->Name = other.Name;
+        this->HitPoints = other.HitPoints;
+        this->EnergyPoints = other.EnergyPoints;
+        this->AttackDamage = other.AttackDamage;
+    }
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& other)
 {
+    std::cout << "Copy assignment operator called\n";
     if (this != &other)
     {
         this->Name = other.Name;
         this->HitPoints = other.HitPoints;
         this->EnergyPoints = other.EnergyPoints;
         this->AttackDamage = other.AttackDamage;
-        std::cout << "Copy assignment operator called\n";
     }
     return *this;
 }
@@ -43,43 +42,43 @@ std::string ClapTrap::getName(){ return Name; };
 
 void ClapTrap::attack(const std::string& target)
 {
-    if (getHitPoints() == 0)
+    if (HitPoints == 0)
         std::cout << "It's dead\n";
-    else if (getEnergyPoints() > 0)
+    else if (EnergyPoints > 0)
     {
-        setEnergyPoints(getEnergyPoints() - 1);
+        EnergyPoints--;
         std::cout << "ClapTrap " << this->Name << " attacks " << target <<", causing " <<
-            getAttackDamage() << " points of damage!" << std::endl;
+            AttackDamage << " points of damage!" << std::endl;
     }
     else
-        std::cout << "ClapTrap " << getName() << " Can't attack " << target << " No EnergyPoints left\n";
+        std::cout << "ClapTrap " << Name << " Can't attack " << target << " No EnergyPoints left\n";
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-    if (getHitPoints() > 0)
+    if (HitPoints > 0)
     {
-        if (amount > getHitPoints())
-            setHitPoints(0);
+        if (amount > HitPoints)
+            HitPoints = 0;
         else
-            setHitPoints(getHitPoints() - amount);
-        std::cout << "ClapTrap " << getName() << " Take " << amount << " points of damage!\n";
+            HitPoints -= amount;
+        std::cout << "ClapTrap " << Name << " Take " << amount << " points of damage!\n";
     }
     else 
-        std::cout << "ClapTrap " << getName() << " Can't take damage because it's dead\n";
+        std::cout << "ClapTrap " << Name << " Can't take damage because it's dead\n";
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-    if (getHitPoints() == 0)
-        std::cout << "ClapTrap " << getName() << " Can't repair because it have 0 HitPoints\n";
-    else if (getEnergyPoints() == 0)
-        std::cout << "ClapTrap " << getName() << " Can't repair because it have 0 EnergyPoints\n";
+    if (HitPoints == 0)
+        std::cout << "ClapTrap " << Name << " Can't repair because it have 0 HitPoints\n";
+    else if (EnergyPoints == 0)
+        std::cout << "ClapTrap " << Name << " Can't repair because it have 0 EnergyPoints\n";
     else
     {
-        setEnergyPoints(getEnergyPoints() - 1);
-        setHitPoints(getHitPoints() + amount);
-        std::cout << "ClapTrap " << getName() << " regain " << amount << " of HitPoints\n";
+        EnergyPoints--;
+        HitPoints += amount;
+        std::cout << "ClapTrap " << Name << " regain " << amount << " of points\n";
     }
 }
 
